f927376d2de5163fb4b8e316409b97e76c99e698
hellrookie <hellrookie@163.com>
2017-11-17 17:34:37 +0800

    1. edit webpack config file to support ES6 and react. 2. add topNav files

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,9 @@
 var path = require('path');
 fs = require('fs');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    entry: getEntry(),
-    output: {
-        path: path.join(__dirname, "buid/js/"),
-        publicPath: "buid/js/",
-        filename: "[name].js",
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-            }
-        ]
-    }
-};
-
-function getEntry() {
-    return {
-        "components": getFiles('./src/components', '.js')
-    }
-}
-
-function getFiles(folderPath, ext) {
-    var result = [];
-
+var entries = {};
+function getEntry(folderPath, ext) {
     function finder(fp) {
         var files = fs.readdirSync(fp);
         files.forEach(function (val) {
@@ -35,7 +12,7 @@ function getFiles(folderPath, ext) {
             //if(stats.isDirectory()) finder(fPath); // get files recursively
             if (stats.isFile() && fPath.endsWith(ext)) {
                 console.log(fPath);
-                result.push(fPath);
+                entries[val.substr(0, val.lastIndexOf('.'))] = fPath;
             }else{
                 console.log('exclude file: ' + fPath);
             }
@@ -44,5 +21,43 @@ function getFiles(folderPath, ext) {
     console.log("************* begin to find entry files *****************");
     finder(folderPath);
     console.log("************* find entry files finish *****************");
-    return result;
+    if(entries.length <= 0){
+        console.log('Warning: no file find to build!!!');
+    }
+    return entries;
 }
+
+var plugins = [];
+function getPlugins() {
+    for(var name in entries){
+        plugins.push(
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'template.html'),
+                filename: '../'+name+'.html',
+                chunks: [name],
+                hash:true,
+            }));
+    }
+    return plugins;
+}
+
+
+module.exports = {
+    entry: getEntry('./src', '.jsx'),
+    output: {
+        path: path.join(__dirname, "build/js/"),
+        //publicPath: "build/js/",
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ]
+    },
+    plugins:getPlugins(),
+    devtool: 'source-map',
+};
