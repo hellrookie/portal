8f5741a0af1b922f58439dee0dc40bf5a7abe2c7
hellrookie <hellrookie@163.com>
2017-11-27 16:00:01 +0800

    1. add jquery and bootstrap lib 2. update config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 var path = require('path');
 fs = require('fs');
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
 
-var entries = {};
+var entries = {
+    vendorDOM:['react', 'react-dom'],
+    vendorThird:['jquery', 'bootstrap'],
+};
+
+var entryFiles = [];
+
 function getEntry(folderPath, ext) {
     function finder(fp) {
         var files = fs.readdirSync(fp);
@@ -12,30 +20,49 @@ function getEntry(folderPath, ext) {
             //if(stats.isDirectory()) finder(fPath); // get files recursively
             if (stats.isFile() && fPath.endsWith(ext)) {
                 console.log(fPath);
-                entries[val.substr(0, val.lastIndexOf('.'))] = fPath;
-            }else{
+                var fileName = val.substr(0, val.lastIndexOf('.'));
+                entryFiles.push(fileName);
+                entries[fileName] = fPath;
+            } else {
                 console.log('exclude file: ' + fPath);
             }
         });
     }
+
     console.log("************* begin to find entry files *****************");
     finder(folderPath);
     console.log("************* find entry files finish *****************");
-    if(entries.length <= 0){
+    if (entries.length <= 0) {
         console.log('Warning: no file find to build!!!');
     }
     return entries;
 }
 
-var plugins = [];
+var plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendorDOM', 'vendorThird'],
+        minChunks: Infinity
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        chunks: ['vendorDOM', 'vendorThird'],
+    }),
+    new CleanWebpackPlugin(['build'],
+        {
+            root: __dirname,
+            verbose: true,
+            dry: false,
+        })
+];
+
 function getPlugins() {
-    for(var name in entries){
+    for (var pos in entryFiles) {
         plugins.push(
             new HtmlWebpackPlugin({
                 template: path.join(__dirname, 'template.html'),
-                filename: '../'+name+'.html',
-                chunks: [name],
-                hash:true,
+                filename: '../' + entryFiles[pos] + '.html',
+                chunks: ['manifest', 'vendorDOM', 'vendorThird', entryFiles[pos]],
+                chunksSortMode: 'manual'
             }));
     }
     return plugins;
@@ -47,21 +74,34 @@ module.exports = {
     output: {
         path: path.join(__dirname, "build/js/"),
         //publicPath: "build/js/",
-        filename: "[name].js",
+        filename: "[name].[chunkhash].js",
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
+                exclude: /node_modules/,
                 use: 'babel-loader',
             },
             {
                 test: /\.(css|scss)$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test:require.resolve('jquery'),
+                use:[
+                    {
+                        loader: 'expose-loader',
+                        options: 'jQuery'
+                    },
+                    {
+                        loader: 'expose-loader',
+                        options: '$'
+                    }
+                ]
             }
         ]
     },
-    plugins:getPlugins(),
+    plugins: getPlugins(),
     devtool: 'source-map',
 };
