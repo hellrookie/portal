b193832c347d5603d8e66a0e76dde9aed8aa770f
hellrookie <hellrookie@163.com>
2018-03-06 14:16:46 -0500

    Get image information for sliders from php

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 import TopNavigation from './components/nav/topNav.jsx'
 import Slider from '../hellrookie.util/slider/slider.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
 
-/*let app = express();
-app.post('../resource/config.json', function(req, res){
-    fs.readfile
-})*/
-let items = []
+$.post("/server/bin/configuration.php", {path:"/slider/items"}, (data, status)=>{
+    if(status == 'success') {
+        let items = $.parseJSON(data);
+        ReactDOM.render(
+            <div>
+                <TopNavigation>
+                </TopNavigation>
+                <div className="container" style={{height:'300px', width:'400px', padding:'0', borderRadius:'20px'}}>
+                    <Slider items={items}/>
+                </div>
+            </div>,
+            document.getElementById('root')
+        );
+    }
+})
 
-ReactDOM.render(
-    <div>
-        <TopNavigation>
-        </TopNavigation>
-        <div className="container" style={{height:'300px', width:'400px', padding:'0', borderRadius:'20px'}}>
-            <Slider items={items}/>
-        </div>
-    </div>,
-    document.getElementById('root')
-);
